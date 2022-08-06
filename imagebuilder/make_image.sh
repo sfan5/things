c28dd33598a014ed6ddb5fd27b698aa56dba701a
sfan5 <sfan5@live.de>
2022-08-06 18:09:51 +0200

    add

---
+++ imagebuilder/make_image.sh
@@ -0,0 +1,104 @@
+#!/bin/bash -e
+
+msg() { printf '\e[35;1m::\e[0m %s\n' "$1"; }
+die() { msg "ERROR: $1"; exit 1; }
+
+device=quartz64-b
+target=./output/alarm-${device}-latest.img.gz
+
+[ $EUID -eq 0 ] || die "Root privileges required"
+
+if [[ "$(uname -m)" != aarch64 ]]; then
+	die TODO # host -> vm (where the script runs) -> chroot (target)
+fi
+
+for exe in sfdisk losetup mkfs.vfat mkfs.ext4 bsdtar pv; do
+	command -v "$exe" >/dev/null || die "Missing $exe"
+done
+
+# host -> chroot (target)
+
+source=./output/alarm-aarch64-latest.tar.gz
+[ -s "$source" ] || die "Missing the rootfs at $source"
+
+loopdev=
+mntdir=
+tmpfile=
+cleanup_mount () {
+	if [ -n "$mntdir" ]; then
+		umount -R "$mntdir"
+		rmdir "$mntdir"
+		mntdir=
+	fi
+}
+_cleanup () {
+	set +e
+	cleanup_mount
+	[ -n "$loopdev" ] && { losetup -d "$loopdev"; loopdev=; }
+	[ -n "$tmpfile" ] && { rm "$tmpfile"; tmpfile=; }
+}
+trap _cleanup EXIT
+
+truncate -s 4G "./blk$$.bin"
+tmpfile=./blk$$.bin
+
+msg "Partitioning"
+sfdisk "$tmpfile" <<"PARTS"
+label: gpt
+unit: sectors
+first-lba: 34
+sector-size: 512
+
+start=64, size=   1M, type=L
+start= +, size=   8M, type=L, name="uboot"
+start= +, size=  32M, type=L, name="env"
+start= +, size= 512M, type=U, name="efi"
+start= +, size=    +, type=L, name="root"
+PARTS
+
+loopdev=$(losetup -fP --show "$tmpfile")
+echo "Loop device: $loopdev"
+
+mkfs.vfat -F32 -n efi "${loopdev}p4"
+mkfs.ext4 -L rootfs "${loopdev}p5"
+
+# TODO: a bootloader package should handle this
+cat <bins/idblock.bin >"${loopdev}p1"
+cat <bins/uboot.img >"${loopdev}p2"
+
+mkdir "./mnt$$"
+mntdir=./mnt$$
+
+mount "${loopdev}p5" "$mntdir" -o noatime
+install -d -m 755 "$mntdir/boot"
+mount "${loopdev}p4" "$mntdir/boot" -o noatime
+
+msg "Extracting"
+bsdtar -xpf "$source" -C "$mntdir"
+
+mkdir -p "$mntdir/boot/extlinux"
+install -T -m 644 /dev/stdin "$mntdir/boot/extlinux/extlinux.conf" <<"BOOTMENU"
+default l0
+menu title Quartz64 Boot Menu
+prompt 0
+timeout 50
+
+label l0
+menu label Arch Linux
+linux /Image
+fdt /dtbs/rockchip/rk3566-quartz64-b.dtb
+append initrd=/initramfs-linux.img earlycon=uart8250,mmio32,0xfe660000 console=ttyS2,1500000n8 root=LABEL=rootfs rw rootwait
+BOOTMENU
+
+# TODO: should install the right kernel instead of this
+cp bins/rk3566-quartz64-b.dtb "$mntdir/boot/dtbs/rockchip/"
+
+sync "$mntdir/"
+cleanup_mount
+
+msg "Compressing"
+pv "$loopdev" | gzip >"$target"
+
+msg "Done"
+ls -l "$target"
+exit 0
---
+++ imagebuilder/make_rootfs.sh
@@ -0,0 +1,125 @@
+#!/bin/bash -e
+
+msg() { printf '\e[35;1m::\e[0m %s\n' "$1"; }
+die() { msg "ERROR: $1"; exit 1; }
+
+target=./output/alarm-aarch64-latest.tar.gz
+
+[ $EUID -eq 0 ] || die "Root privileges required"
+
+if [[ "$(uname -m)" != aarch64 ]]; then
+	die TODO # host -> vm (where the script runs) -> chroot (target)
+fi
+
+if ! command -v pacstrap; then
+	die TODO # should this be supported?
+fi
+
+for exe in unshare bsdtar; do
+	command -v "$exe" >/dev/null || die "Missing $exe"
+done
+
+# host -> chroot (target)
+
+
+tmpdir=
+active_mounts=()
+cleanup_mounts () {
+	if [ ${#active_mounts[@]} -gt 0 ]; then
+		umount "${active_mounts[@]}"
+	fi
+	active_mounts=()
+}
+_cleanup () {
+	set +e
+	cleanup_mounts
+	[ -n "$tmpdir" ] && { rm -rf "$tmpdir"; tmpdir=; }
+}
+trap _cleanup EXIT
+
+install -d -m 755 "./tmp$$"
+tmpdir=./tmp$$
+
+pkgs=(
+	base
+	linux-aarch64 linux-firmware
+	openssh net-tools
+	netctl # for wifi-menu
+	nano vi
+)
+
+msg "Running pacstrap"
+pacstrap -c -GM "$tmpdir" "${pkgs[@]}"
+
+msg "Applying customizations"
+target_chroot () {
+	PATH=/usr/bin unshare -R "$tmpdir" -- "$@" </dev/null
+}
+add_mount () {
+	local from="$1"
+	local to="$2"
+	shift; shift
+	mount "$from" "$tmpdir$to" "$@"
+	active_mounts+=("$tmpdir$to")
+}
+
+add_mount proc /proc -t proc
+
+# FIXME: should set up minimal amount of devices instead
+add_mount dev /dev -t devtmpfs
+install -d 1777 "$tmpdir/dev/shm"
+
+add_mount tmpfs /tmp -t tmpfs -o nosuid,nodev,mode=1777
+add_mount tmpfs /run -t tmpfs -o nosuid,nodev
+
+# regenerate initramfs
+target_chroot mkinitcpio -P
+
+# services
+target_chroot systemctl enable sshd.service systemd-{resolved,networkd,timesyncd}.service
+
+# system config
+# TODO: C.UTF-8 should work too
+target_chroot bash -c '
+echo alarm | install -T -m 644 /dev/stdin /etc/hostname
+echo LANG=C | install -T -m 644 /dev/stdin /etc/locale.conf
+'
+
+# network
+target_chroot bash -c '
+for prefix in en eth; do
+	install -T -m 644 /dev/stdin /etc/systemd/network/${prefix}.network <<"STOP"
+[Match]
+Name=${prefix}*
+
+[Network]
+DHCP=yes
+DNSSEC=no
+STOP
+done
+rm /etc/resolv.conf
+ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+'
+
+# user accounts
+target_chroot bash -c '
+useradd -m alarm
+printf "%s\n" root:root alarm:alarm | chpasswd
+'
+
+# cleanup
+target_chroot bash -c '
+yes | pacman -Scc
+rm -r /root/.gnupg
+rm /var/log/pacman.log /etc/machine-id
+'
+
+cleanup_mounts
+
+rm -f "$target"
+msg "Creating archive"
+bsdtar -cz --numeric-owner -f "$target" -C "$tmpdir" .
+
+msg "Done"
+ls -l "$target"
+exit 0
